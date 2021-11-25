d00a1f5f39abf27a33cb48a0db4f63af7a1c99ba
Juan Martha <juan.martha.mt@correo.usa.edu.co>
2021-11-24 19:06:32 -0500

    first commit

---
+++ js/validarlogin.js
@@ -0,0 +1,50 @@
+function validar() {
+  const usuario = document.getElementById('usuario');
+  const clave = document.getElementById('clave');
+
+  if (usuario.value.length == 0) {
+    alert("Ingrese un usuario")
+    usuario.focus()
+    return 0;
+  }
+
+  if (clave.value.length == 0) {
+    alert("Ingrese una contraseña")
+    clave.focus()
+    return 0;
+
+  }
+
+  let credentials = {
+    usuario: $("#usuario").val(),
+    clave: $("#clave").val()
+  };
+
+  $.ajax({
+    type: 'GET',
+    contentType: "application/json; charset=utf-8",
+    dataType: 'JSON',
+    // data: JSON.stringify(credentials),
+
+    url: "http://152.67.132.140:8080/api/user/" + credentials.usuario + "/" + credentials.clave,
+
+    success: function (response) {
+      if (response.name == 'NO DEFINIDO') {
+        alert('Usuario o clave incorrectos!');
+        return;
+      }
+      console.log(response);
+      console.log("Bienvenido");
+      alert("Acabas de iniciar sesion");
+      window.location.reload()
+    },
+
+    error: function (jqXHR, textStatus, errorThrown) {
+      window.location.reload()
+      alert("no existe usuario");
+    }
+  }
+  );
+
+
+}
